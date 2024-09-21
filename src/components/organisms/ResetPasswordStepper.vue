1f567b5abc066c5d695a353ddee3c322a920aca2
Nikita Golubev <nekitgo80@gmail.com>
2024-09-21 12:47:24 +0300

    Add linter support

--- src/components/organisms/ResetPasswordStepper.vue
+++ src/components/organisms/ResetPasswordStepper.vue
@@ -41,71 +41,141 @@ const onClickFinalizeReset = async(activateCallback) => {
 </script>
 
 <template>
-  <Stepper value="1" linear="false">
+  <Stepper
+    value="1"
+    linear="false"
+  >
     <StepList>
-      <Step value="1">Ввод email</Step>
-      <Step value="2">Верификация</Step>
-      <Step value="3">Установка нового пароля</Step>
+      <Step value="1">
+        Ввод email
+      </Step>
+      <Step value="2">
+        Верификация
+      </Step>
+      <Step value="3">
+        Установка нового пароля
+      </Step>
     </StepList>
     <StepPanels>
-      <StepPanel v-slot="{ activateCallback }" value="1">
+      <StepPanel
+        v-slot="{ activateCallback }"
+        value="1"
+      >
         <Card>
-          <template #title>Reset Your Password?</template>
+          <template #title>
+            Reset Your Password?
+          </template>
           <template #subtitle>
-            <div class="mt-2 mb-2">Enter your email for instructions</div>
+            <div class="mt-2 mb-2">
+              Enter your email for instructions
+            </div>
           </template>
           <template #content>
             <div class="flex flex-col gap-5">
               <div class="flex flex-col gap-2">
                 <label for="email">Registered Email</label>
-                <InputText id="email" v-model="email" aria-describedby="Email" />
+                <InputText
+                  id="email"
+                  v-model="email"
+                  aria-describedby="Email"
+                />
               </div>
-              <Button @click="onClickRequestEmail(activateCallback)" label="Reset password" severity="primary" />
-              <Button @click="onClickBackToLogin" label="Back to Login" severity="secondary" />
+              <Button
+                label="Reset password"
+                severity="primary"
+                @click="onClickRequestEmail(activateCallback)"
+              />
+              <Button
+                label="Back to Login"
+                severity="secondary"
+                @click="onClickBackToLogin"
+              />
             </div>
           </template>
         </Card>
       </StepPanel>
-      <StepPanel v-slot="{ activateCallback }" value="2">
+      <StepPanel
+        v-slot="{ activateCallback }"
+        value="2"
+      >
         <Card class="flex">
-          <template #title>Verification</template>
+          <template #title>
+            Verification
+          </template>
           <template #subtitle>
-            <div class="mt-2 mb-2">We have sent code to you email</div>
+            <div class="mt-2 mb-2">
+              We have sent code to you email
+            </div>
           </template>
           <template #content>
             <div class="flex flex-col gap-5">
-              <InputOtp :length="6" v-model="otp"/>
+              <InputOtp
+                v-model="otp"
+                :length="6"
+              />
               <div class="flex gap-3">
-                <Button @click="onClickVerifyToken(activateCallback)" label="Verify" severity="primary" />
-                <Button @click="activateCallback('1')" label="Cancel" severity="secondary" />
+                <Button
+                  label="Verify"
+                  severity="primary"
+                  @click="onClickVerifyToken(activateCallback)"
+                />
+                <Button
+                  label="Cancel"
+                  severity="secondary"
+                  @click="activateCallback('1')"
+                />
               </div>
             </div>
           </template>
         </Card>
       </StepPanel>
-      <StepPanel v-slot="{ activateCallback }" value="3">
+      <StepPanel
+        v-slot="{ activateCallback }"
+        value="3"
+      >
         <Card>
-          <template #title>Set new password</template>
+          <template #title>
+            Set new password
+          </template>
           <template #content>
             <div class="flex flex-col gap-5">
               <div class="flex flex-col gap-2">
                 <label for="password">New password</label>
-                <InputText id="password" v-model="password" />
+                <InputText
+                  id="password"
+                  v-model="password"
+                />
               </div>
               <div class="flex flex-col gap-2">
                 <label for="password_confirmation">Confirm password</label>
-                <InputText id="password_confirmation" v-model="passwordConfirmation" />
+                <InputText
+                  id="password_confirmation"
+                  v-model="passwordConfirmation"
+                />
               </div>
-              <Button @click="onClickFinalizeReset(activateCallback)" label="Confirm" severity="primary" />
+              <Button
+                label="Confirm"
+                severity="primary"
+                @click="onClickFinalizeReset(activateCallback)"
+              />
             </div>
           </template>
         </Card>
       </StepPanel>
-      <StepPanel v-slot="{ activateCallback }" value="4">
+      <StepPanel
+        v-slot="{ activateCallback }"
+        value="4"
+      >
         <Card>
-          <template #title>Congratulations!</template>
+          <template #title>
+            Congratulations!
+          </template>
           <template #content>
-            <Button @click="activateCallback('1')" label="Cancel" severity="primary" />
+            <Button
+              label="Cancel"
+              severity="primary"
+              @click="activateCallback('1')"
+            />
           </template>
         </Card>
       </StepPanel>
